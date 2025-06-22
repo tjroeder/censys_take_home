e38f997f8b68614dacf1c99e01e19326508fc56e
Tim Roeder <timothyjroeder@gmail.com>
2025-06-22 16:33:19 -0600

    implement grpc server

--- cache/main.go
+++ cache/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
 
+	"github.com/tjroeder/censys_take_home/cache/grpcserver"
+	"github.com/tjroeder/censys_take_home/cache/internal/cache"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	cache := cache.NewCache()
+	cacheServer := grpcserver.New(cache)
+
+	// Register the gRPC server with the cacheServer
+	grpcserver.RegisterCacheServer(s, cacheServer)
+
+	log.Println("gRPC server listening on :50051")
+	if err = s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 }
